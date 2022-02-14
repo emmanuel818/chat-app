c8ff85f1adf141e5aca47256e5373e139a605888
emmanuel818 <emmanuelg8182001@gmail.com>
2022-02-14 14:20:57 -0600

    First commit

--- App.js
+++ App.js
@@ -1,13 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+//import screens
+import Start from './components/Start';
+import Chat from './components/Chat';
+
+//import react native gesture handler
+import 'react-native-gesture-handler';
+
+//import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+
+
+//Create Navigator
+const Stack = createStackNavigator();
+
+
+export default class ChatApp extends React.Component {
+
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          //First screen to load up upon starting the app
+          initialRouteName="Start"
+        >
+          <Stack.Screen
+            name="Start"
+            component={Start}
+          />
+          <Stack.Screen
+            name="Chat"
+            component={Chat}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
